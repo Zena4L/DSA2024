9832df3af2dd00ff36631e03eb06836c886d670b
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-12-28 14:37:21 +0000

    init: setting up

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.linkedlist.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+        System.out.println(myLinkedList);
+    }
+}
