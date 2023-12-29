9e992bfdc6311226acdeca2ec5699f9b544ffc9e
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-12-29 11:43:37 +0000

    feat: added queue

---
+++ src/main/java/org/example/queue/Main.java
@@ -0,0 +1,7 @@
+package org.example.queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/queue/Queue.java
@@ -0,0 +1,51 @@
+package org.example.queue;
+
+public class Queue {
+
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value) {
+        var newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    class Node{
+        Node next;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void enqueue(int value){
+        var newNode = new Node(value);
+        if (length == 0){
+            first = newNode;
+            last = newNode;
+        }else {
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
+    public Node dequeue(){
+        if(length == 0) return null;
+        Node temp = first;
+
+        if(length == 1){
+            first = null;
+            last = null;
+        }else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return  temp;
+    }
+}
